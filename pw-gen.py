956949afa659f36543a838f35c548c1ebfde92e9
Teemu Kukkonen <teme.kukkonen@gmail.com>
2021-12-07 01:56:22 +0200

    Python password generator

---
+++ pw-gen.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+import random
+import string
+
+default_len = 8
+def get_random_string(length):
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for i in range(length))
+
+a = 8
+try:
+    a = sys.argv[1]
+
+except IndexError as e:
+    a = str(default_len)
+
+
+a = int(a) if a.isdigit() else default_len
+
+print(get_random_string(a))
